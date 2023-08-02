a983927197d1c2a533f0c4ea09d14a5e5b7444f8
yaswanthsomala <somalayaswanth@gmail.com>
2023-08-02 17:45:24 +0530

    app setup

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div style={{ paddingTop: "30px", paddingBottom: "30px" }}></div>;
+};
+
+export default Header;
---
+++ src/components/LeaveForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LeaveForm = () => {
+  return (
+    <div
+      className=""
+      style={{ marginTop: "5px", paddingTop: "5px", backgroundColor: "white" }}
+    ></div>
+  );
+};
+
+export default LeaveForm;
